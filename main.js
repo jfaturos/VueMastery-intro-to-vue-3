ab51eddc7b0492cfc08dec451d32e86c2b14036d
Giannis Fatouros <fatouros.ioannis@gmail.com>
2022-02-25 23:27:25 +0200

    Event Handling

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const app = Vue.createApp({
     data() {
         return {
+            cart: 0,
             product: 'Boots',
             image: './assets/images/socks_green.jpg',
             url: 'https://vuemastery.com',
@@ -8,11 +9,32 @@ const app = Vue.createApp({
             inventory: 100,
             onSale: true,
             details: ['50% cotton', '30% wool', '20% polyester'],
-            variants: [
-                { id:2234, color: 'green' },
-                { id:2235, color: 'blue' }
+            variants: [{
+                    id: 2234,
+                    color: 'green',
+                    image: './assets/images/socks_green.jpg'
+                },
+                {
+                    id: 2235,
+                    color: 'blue',
+                    image: './assets/images/socks_blue.jpg'
+                }
             ],
             sizes: ['S', 'M', 'L', 'XL']
+        }
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1;
+        },
+        updateImage(variantImage) {
+            this.image = variantImage;
+        },
+        removeItem() {
+            if (this.cart === 0)
+                return;
+
+            this.cart -= 1;
         }
     }
 })
